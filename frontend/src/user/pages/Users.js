8057e9bc9981a6ffe9c7820935c6ff3a9b99cf8a
Max Hrytsiuk <maxeemja.inc@gmail.com>
2024-09-07 22:57:55 +0300

    getting users from BE implemented

--- frontend/src/user/pages/Users.js
+++ frontend/src/user/pages/Users.js
@@ -1,19 +1,46 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 import UsersList from '../components/UsersList';
+import ErrorModal from "../../shared/components/UIElements/ErrorModal";
+import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
 
 const Users = () => {
-  const USERS = [
-    {
-      id: 'u1',
-      name: 'Max Schwarz',
-      image:
-        'https://images.pexels.com/photos/839011/pexels-photo-839011.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-      places: 3
-    }
-  ];
+    const [users, setUsers] = useState([])
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
+    useEffect(() => {
+        const sendRequest = async () => {
+            setIsLoading(true);
+            try {
+                const response = await fetch('http://localhost:5000/api/users');
+                const data = await response.json();
+                if (!response.ok) {
+                    throw new Error(data.message)
+                }
+                setUsers(data.users)
+            } catch (e) {
+                setError(e.message || 'Something went wrong :(');
+            }
+            setIsLoading(false)
+        }
+        sendRequest()
+    }, []);
 
-  return <UsersList items={USERS} />;
+    const errorHandler = () => {
+        setError(null)
+    }
+
+    return (
+        <>
+            <ErrorModal error={error} onClear={errorHandler}></ErrorModal>
+            {isLoading && <div className='center'>
+                <LoadingSpinner/>
+            </div>}
+            {!isLoading && users && <UsersList items={users}/>}
+        </>
+    )
+
 };
 
 export default Users;
+ 
