69108f89edc0fb6ba1fcc068576b04cbd87c81f7
Max Hrytsiuk <maxeemja.inc@gmail.com>
2024-09-07 23:56:22 +0300

    get users with new hook, deleted comments

--- frontend/src/user/pages/Auth.js
+++ frontend/src/user/pages/Auth.js
@@ -15,8 +15,6 @@ const Auth = () => {
     const auth = useContext(AuthContext);
     const {isLoading, error, sendRequest, clearError} = useHttp()
     const [isLoginMode, setIsLoginMode] = useState(true);
-    // const [isLoading, setIsLoading] = useState(false);
-    // const [error, setError] = useState(null)
 
     const [formState, inputHandler, setFormData] = useForm({
         email: {
@@ -40,8 +38,7 @@ const Auth = () => {
         }
         setIsLoginMode(prevMode => !prevMode);
     };
-
-
+    
     const authSubmitHandler = async event => {
         event.preventDefault();
 
@@ -74,48 +71,48 @@ const Auth = () => {
     };
 
     return (<>
-            <ErrorModal error={error} onClear={clearError}/>
-            <Card className="authentication">
-                {isLoading && <LoadingSpinner asOverlay/>}
-                <h2>Login Required</h2>
-                <hr/>
-                <form onSubmit={authSubmitHandler}>
-                    {!isLoginMode && (<Input
-                        element="input"
-                        id="name"
-                        type="text"
-                        label="Your Name"
-                        validators={[VALIDATOR_REQUIRE()]}
-                        errorText="Please enter a name."
-                        onInput={inputHandler}
-                    />)}
-                    <Input
-                        element="input"
-                        id="email"
-                        type="email"
-                        label="E-Mail"
-                        validators={[VALIDATOR_EMAIL()]}
-                        errorText="Please enter a valid email address."
-                        onInput={inputHandler}
-                    />
-                    <Input
-                        element="input"
-                        id="password"
-                        type="password"
-                        label="Password"
-                        validators={[VALIDATOR_MINLENGTH(5)]}
-                        errorText="Please enter a valid password, at least 5 characters."
-                        onInput={inputHandler}
-                    />
-                    <Button type="submit" disabled={!formState.isValid}>
-                        {isLoginMode ? 'LOGIN' : 'SIGNUP'}
-                    </Button>
-                </form>
-                <Button inverse onClick={switchModeHandler}>
-                    SWITCH TO {isLoginMode ? 'SIGNUP' : 'LOGIN'}
+        <ErrorModal error={error} onClear={clearError}/>
+        <Card className="authentication">
+            {isLoading && <LoadingSpinner asOverlay/>}
+            <h2>Login Required</h2>
+            <hr/>
+            <form onSubmit={authSubmitHandler}>
+                {!isLoginMode && (<Input
+                    element="input"
+                    id="name"
+                    type="text"
+                    label="Your Name"
+                    validators={[VALIDATOR_REQUIRE()]}
+                    errorText="Please enter a name."
+                    onInput={inputHandler}
+                />)}
+                <Input
+                    element="input"
+                    id="email"
+                    type="email"
+                    label="E-Mail"
+                    validators={[VALIDATOR_EMAIL()]}
+                    errorText="Please enter a valid email address."
+                    onInput={inputHandler}
+                />
+                <Input
+                    element="input"
+                    id="password"
+                    type="password"
+                    label="Password"
+                    validators={[VALIDATOR_MINLENGTH(5)]}
+                    errorText="Please enter a valid password, at least 5 characters."
+                    onInput={inputHandler}
+                />
+                <Button type="submit" disabled={!formState.isValid}>
+                    {isLoginMode ? 'LOGIN' : 'SIGNUP'}
                 </Button>
-            </Card>
-        </>);
+            </form>
+            <Button inverse onClick={switchModeHandler}>
+                SWITCH TO {isLoginMode ? 'SIGNUP' : 'LOGIN'}
+            </Button>
+        </Card>
+    </>);
 };
 
 export default Auth;
--- frontend/src/user/pages/Users.js
+++ frontend/src/user/pages/Users.js
@@ -1,46 +1,37 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from "react";
 
-import UsersList from '../components/UsersList';
+import UsersList from "../components/UsersList";
 import ErrorModal from "../../shared/components/UIElements/ErrorModal";
 import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
+import { useHttp } from "../../shared/hooks/http-hook";
 
 const Users = () => {
-    const [users, setUsers] = useState([])
-    const [isLoading, setIsLoading] = useState(false);
-    const [error, setError] = useState(null);
-    useEffect(() => {
-        const sendRequest = async () => {
-            setIsLoading(true);
-            try {
-                const response = await fetch('http://localhost:5000/api/users');
-                const data = await response.json();
-                if (!response.ok) {
-                    throw new Error(data.message)
-                }
-                setUsers(data.users)
-            } catch (e) {
-                setError(e.message || 'Something went wrong :(');
-            }
-            setIsLoading(false)
-        }
-        sendRequest()
-    }, []);
+  const { isLoading, error, sendRequest, clearError } = useHttp();
+  const [users, setUsers] = useState([]);
 
-    const errorHandler = () => {
-        setError(null)
-    }
+  useEffect(() => {
+    const getUsers = async () => {
+      try {
+        const responseData = await sendRequest(
+          "http://localhost:5000/api/users",
+        );
+        setUsers(responseData.users);
+      } catch (e) {}
+    };
+    getUsers();
+  }, [sendRequest]);
 
-    return (
-        <>
-            <ErrorModal error={error} onClear={errorHandler}></ErrorModal>
-            {isLoading && <div className='center'>
-                <LoadingSpinner/>
-            </div>}
-            {!isLoading && users && <UsersList items={users}/>}
-        </>
-    )
-
+  return (
+    <>
+      <ErrorModal error={error} onClear={clearError}></ErrorModal>
+      {isLoading && (
+        <div className="center">
+          <LoadingSpinner />
+        </div>
+      )}
+      {!isLoading && users && <UsersList items={users} />}
+    </>
+  );
 };
 
 export default Users;
- 
