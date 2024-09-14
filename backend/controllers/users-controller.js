df67603702cc79ca58b4e8e92512bef1a152fcfa
Maksym Hrytsiuk <maksym_hrytsiuk@epam.com>
2024-09-15 01:25:54 +0300

    [BE] password hashing added, jwt + bcrypt

--- backend/controllers/users-controller.js
+++ backend/controllers/users-controller.js
@@ -1,68 +1,118 @@
-const HttpError = require("../models/http-error");
-const {validationResult} = require("express-validator");
-const User = require("../models/user");
-const mongoose = require("mongoose");
+const HttpError = require('../models/http-error');
+const { validationResult } = require('express-validator');
+const User = require('../models/user');
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const getUsers = async (req, res, next) => {
-    let users;
-    try {
-        users = await User.find({}, "-password");
-    } catch (e) {
-        return next(new HttpError("Fetching users failed", 500));
-    }
+	let users;
+	try {
+		users = await User.find({}, '-password');
+	} catch (e) {
+		return next(new HttpError('Fetching users failed', 500));
+	}
 
-    res.json({users: users});
+	res.json({ users: users });
 };
 const signup = async (req, res, next) => {
-    const errors = validationResult(req);
-    if (errors.errors.length) {
-        console.log(errors);
-        return next(new HttpError("Invalid inputs passed", 422));
-    }
+	const errors = validationResult(req);
+	if (errors.errors.length) {
+		console.log(errors);
+		return next(new HttpError('Invalid inputs passed', 422));
+	}
 
-    const {name, email, password} = req.body;
+	const { name, email, password } = req.body;
 
-    let existingUser;
-    try {
-        existingUser = await User.findOne({email: email});
-    } catch (e) {
-        return next(new HttpError("Signing up failed", 500));
-    }
+	let existingUser;
+	try {
+		existingUser = await User.findOne({ email: email });
+	} catch (e) {
+		return next(new HttpError('Signing up failed', 500));
+	}
 
-    if (existingUser) {
-        return next(new HttpError("User already exists", 401));
-    }
+	if (existingUser) {
+		return next(new HttpError('User already exists', 401));
+	}
 
-    const createdUser = new User({
-        name,
-        email,
-        password,
-        image:
-            "https://upload.wikimedia.org/wikipedia/commons/9/99/Sample_User_Icon.png",
-        places: [],
-    });
+	let hashedPassword;
+	try {
+		hashedPassword = await bcrypt.hash(password, 12);
+	} catch (error) {
+		const err = new HttpError(error.message, 500);
+		return next(err);
+	}
 
-    try {
-        await createdUser.save();
-    } catch (e) {
-        return next(new HttpError("Signing up failed", 500));
-    }
-    res.status(201).json({user: createdUser.toObject({getters: true})});
+	const createdUser = new User({
+		name,
+		email,
+		password: hashedPassword,
+		image:
+			'https://upload.wikimedia.org/wikipedia/commons/9/99/Sample_User_Icon.png',
+		places: []
+	});
+
+	try {
+		await createdUser.save();
+	} catch (e) {
+		return next(new HttpError('Signing up failed', 500));
+	}
+
+	let token;
+	try {
+		token = jwt.sign(
+			{ userId: createdUser.id, email: createdUser.email },
+			'supersecret',
+			{ expiresIn: '24h' }
+		);
+	} catch (error) {
+		return next(new HttpError(error.message, 500));
+	}
+
+	res
+		.status(201)
+		.json({ userId: createdUser.id, email: createdUser.email, token: token });
 };
 
 const login = async (req, res, next) => {
-    const {email, password} = req.body;
-    const existingUser = await User.findOne({email: email})
-    
-    if (!existingUser) {
-        return next(new HttpError("User not exist", 500));
-    }
+	const { email, password } = req.body;
+	const existingUser = await User.findOne({ email: email });
 
-    if (existingUser && existingUser.password !== password) {
-        return next(new HttpError("Wrong password", 401));
-    }
+	if (!existingUser) {
+		return next(new HttpError('User not exist', 500));
+	}
 
-    res.json({message: "Logged in", user: existingUser.toObject({getters: true})});
+	if (existingUser && existingUser.password !== password) {
+		return next(new HttpError('Wrong password', 401));
+	}
+
+	let isValidPassword = false;
+	try {
+		isValidPassword = await bcrypt.compare(password, existingUser.password);
+	} catch (error) {
+		return next(new HttpError('Could not log you in, check creds', 500));
+	}
+
+	if (!isValidPassword) {
+		return next(new HttpError('Invalid password', 401));
+	}
+
+	let token;
+	try {
+		token = jwt.sign(
+			{ userId: existingUser.id, email: existingUser.email },
+			'supersecret',
+			{ expiresIn: '24h' }
+		);
+	} catch (error) {
+		return next(new HttpError(error.message, 500));
+	}
+
+	res.json({
+		userId: existingUser.id,
+		email: existingUser.email,
+		token: token
+	});
 };
 
-module.exports = {getUsers, signup, login};
+module.exports = { getUsers, signup, login };
--- backend/routes/places-routes.js
+++ backend/routes/places-routes.js
@@ -1,33 +1,33 @@
-const express = require("express");
-const { check } = require("express-validator");
+const express = require('express');
+const { check } = require('express-validator');
 const router = express.Router();
 const {
-  getPlaceById,
-  getPlacesByUserId,
-  createPlace,
-  getPlaces,
-  updatePlace,
-  deletePlace,
-} = require("../controllers/places-controller");
+	getPlaceById,
+	getPlacesByUserId,
+	createPlace,
+	getPlaces,
+	updatePlace,
+	deletePlace
+} = require('../controllers/places-controller');
+const checkAuth = require('../middleware/check-auth');
 
-router.get("/", getPlaces);
+router.get('/', getPlaces);
 
-router.get("/:pid", getPlaceById);
+router.get('/:pid', getPlaceById);
 
-router.get("/user/:uid", getPlacesByUserId);
+router.use(checkAuth);
 
-router.post(
-  "/",
-  createPlace,
-);
+router.get('/user/:uid', getPlacesByUserId);
+
+router.post('/', createPlace);
 
 router.patch(
-  "/:pid",
-  check(["title", "description"]).not().isEmpty(),
-  check("description").isLength({ min: 5 }),
-  updatePlace,
+	'/:pid',
+	check(['title', 'description']).not().isEmpty(),
+	check('description').isLength({ min: 5 }),
+	updatePlace
 );
 
-router.delete("/:pid", deletePlace);
+router.delete('/:pid', deletePlace);
 
 module.exports = router;
